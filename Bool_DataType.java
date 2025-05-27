7445b9a5daafc00db529425faa68a966e4a37252
sameerbasha17 <sksameerbasha703@gmail.com>
2025-05-27 14:12:45 +0530

    05,ila, Understanding Boolean Datatype

---
+++ Bool_DataType.java
@@ -0,0 +1,9 @@
+public class Bool_DataType {
+    public static void main(String [] args)
+    {
+        boolean b1=true;
+        boolean b2=false;
+        System.out.println("i am a fool? " + b1);
+        System.out.println("I am atomic? " +b2);
+    }
+}
