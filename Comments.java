fb05c0577f9f64fc111f81164f33e7a5d6a131a9
sameerbasha17 <sksameerbasha703@gmail.com>
2025-05-27 14:02:11 +0530

    02,ila,let's go...

---
+++ Comments.java
@@ -0,0 +1,19 @@
+public class Comments {
+    public static void main(String [] args){
+        System.out.println("There are three types of commands .\n There are :");
+        System.out.println("1.Single line command");
+        //This is a single line comment.
+        System.out.println("2.Multi line command");
+        /* This is a multi line comment,
+        which means where we can write multiple 
+        comment lines */
+        System.out.println("3.Documentation comment");
+        /** 
+         * i don't know
+         * very much about this
+         * i will figure it out later.
+         */
+
+    }
+}
+
